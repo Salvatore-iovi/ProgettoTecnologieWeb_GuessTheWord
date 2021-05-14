5fc282345b138a24b7169b68e2e0c4f10d650267
Salvatore-iovi <salvatore.iovinella001@studenti.uniparthenope.it>
2021-05-14 16:53:44 +0200

    aggiunti i suggerimenti

--- Liv1.js
+++ Liv1.js
@@ -1,7 +1,10 @@
 function mostraMessaggio()
 {
-    messaggio=confirm("vuoi davvero tornare alla home tutti i progressi andranno persi")
+    messaggio=confirm("vuoi davvero tornare alla home? Tutti i progressi andranno persi")
     if(messaggio==true){
         window.history.go(-1)
     }
+}
+function suggerimenti() {
+    confirm("Qui ci sono i suggerimenti")
 }
