fee4f956cf66e37b9f3687f05d210d0d71f9cdb8
Salvatore-iovi <salvatore.iovinella001@studenti.uniparthenope.it>
2021-05-18 18:15:43 +0200

    risolta finalmente la funzione che dava fastidio implementate le immagini siamo a buon punto

--- Liv2/Liv2.js
+++ Liv2/Liv2.js
@@ -2,24 +2,22 @@ function mostraMessaggio()
 {
     messaggio=confirm("vuoi davvero tornare alla home? Tutti i progressi andranno persi")
     if(messaggio==true){
-        window.history.go(-1)
+        window.history.go(-2)
     }
 }
 function suggerimenti() {
-    alert("Indaga sui crimini")
+    alert("Qui andranno i suggerimenti")
 }
 function controlloRisposta()
 {
-    let risposta_esatta="investigatore"
+    let risposta_esatta="parola"
     let display=document.getElementById("risposta_utente")
     let displayValue=display.value
-    console.log(":::" +displayValue)
     if(displayValue===risposta_esatta)
     {
         alert("Risposta esatta")
-        location.href="http://localhost:63342/GuessTheWord/Liv2/Livello2.html?_ijt=n2anun9vr4ue7j1g0vomjdtnf9"
+        location.href=""
     }else{
         alert("Risposta errata...Riprova")
     }
 }
-
