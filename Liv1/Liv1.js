c551a4e39aa562ca55f96e8a45c7502629618f9f
Salvatore-iovi <salvatore.iovinella001@studenti.uniparthenope.it>
2021-05-19 12:51:26 +0200

    aggiunte animazioni immagini eliminata provvisoriamente la navbar

--- Liv1/Liv1.js
+++ Liv1/Liv1.js
@@ -2,7 +2,7 @@ function mostraMessaggio()
 {
     messaggio=confirm("vuoi davvero tornare alla home? Tutti i progressi andranno persi")
     if(messaggio==true){
-        window.history.go(-1)
+        location.href="http://localhost:63342/GuessTheWord/Index/index.html?_ijt=7b57a8tekicc8e6vr3irjusvh5"
     }
 }
 function suggerimenti() {
@@ -13,7 +13,7 @@ function controlloRisposta()
     let risposta_esatta="investigatore"
     let display=document.getElementById("risposta_utente")
     let displayValue=display.value
-    if(displayValue===risposta_esatta)
+    if(displayValue.toLowerCase()===risposta_esatta)
     {
         alert("Risposta esatta")
         location.href="http://localhost:63342/GuessTheWord/Liv2/Livello2.html?_ijt=n2anun9vr4ue7j1g0vomjdtnf9"
--- Liv3/Liv3.js
+++ Liv3/Liv3.js
@@ -6,17 +6,17 @@ function mostraMessaggio()
     }
 }
 function suggerimenti() {
-    alert("Non brillano di luce propria")//suggerimenti per indovinare la parola
+    alert("In passato era un vichingo")//suggerimenti per indovinare la parola
 }
 function controlloRisposta()
 {
-    let risposta_esatta="pianeti"//risposta corretta
+    let risposta_esatta="scandinavo"//risposta corretta
     let display=document.getElementById("risposta_utente")//vado ad estrarre il contenuto della variabile risposta utente
     let displayValue=display.value
     if(displayValue.toLowerCase()===risposta_esatta)//verifico se la risposta utente è uguale a risposta esatta
     {
         alert("Risposta esatta")
-        location.href="http://localhost:63342/GuessTheWord/Liv3/Livello3.html?_ijt=k4kju5rnqjl2antg9bm6vh6df9"
+        location.href=""
     }else{
         alert("Risposta errata...Riprova")
         display.value=""
