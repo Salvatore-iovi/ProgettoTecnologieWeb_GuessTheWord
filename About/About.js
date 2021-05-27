d5a7e1813baed6df5d6e1b28d39098849d2aadd7
Salvatore-iovi <salvatore.iovinella001@studenti.uniparthenope.it>
2021-05-27 17:45:03 +0200

    sistemata pagina about e aggiunte le opacità buona visione

---
+++ About/About.js
@@ -0,0 +1,16 @@
+function openSalvatorePopup()
+{
+    document.getElementById("mysalvatorepopup").style.display="block";
+}
+function closeSalvatorePopup()
+{
+    document.getElementById("mysalvatorepopup").style.display="none";
+}
+function openFrancescoPopup()
+{
+    document.getElementById("myfrancescopopup").style.display="block";
+}
+function closeFrancescoPopup()
+{
+    document.getElementById("myfrancescopopup").style.display="none";
+}
