400c28cca26c02e9aa05aa7ef821e6fcc22702de
Salvatore-iovi <salvatore.iovinella001@studenti.uniparthenope.it>
2021-06-03 18:00:31 +0200

    risolvere il fatto delle monete

--- Liv1/Liv1.js
+++ Liv1/Liv1.js
@@ -1,5 +1,6 @@
 var cont=0 //variabile per verificare se si usano i suggerimenti
 var tentativi=3
+var error=false
 function controlloRisposta()
 {
     let risposta_esatta="investigatore"
@@ -12,14 +13,26 @@ function controlloRisposta()
         assegnaMonete()
         location.href="http://localhost:63342/GuessTheWord/Liv2/Livello2.html?_ijt=n2anun9vr4ue7j1g0vomjdtnf9"
     }else{
-        tentativi--
-        alert("Risposta errata...Ti restano: " +tentativi+ " tentativi")
-        display.value=""
-
+        if(displayValue==' ' || displayValue=="")
+        {
+            alert("inserire almeno un carattere")
+            display.value=""
+        }
+        else{
+            tentativi--
+            alert("Risposta errata...Ti restano: " +tentativi+ " tentativi")
+            display.value=""
+        }
     if(tentativi==0)
     {
-        alert("Hai perso")
-        location.href="http://localhost:63342/GuessTheWord/Index/index.html?_ijt=67i1nsgsb6dook9fnd981spm60"
+        var risposta=confirm("Hai perso...vuoi ricominciare la partita?")
+        if(risposta==true)
+        {
+            location.href="http://localhost:63342/GuessTheWord/Liv1/Livello1.html?_ijt=1u1d0gkge0ttvf1l4j6flf4mvg"
+        }
+        else{
+            location.href="http://localhost:63342/GuessTheWord/Index/index.html?_ijt=c6apgdbbfq3podof7b2sd7khu7"
+        }
     }
     }
 }
@@ -29,13 +42,15 @@ function openSuggerimenti()
     monete = monete - 50
     localStorage.setItem("coins",monete)
     document.getElementById("monete_gioco").innerHTML=monete
-    if(monete==0)
+    if(monete>0)
     {
-        document.getElementById("suggerimenti").disabled=true
-        alert("Non hai abbastanza monete per poter utilizzare i suggerimenti")
+        document.getElementById("myForm").style.display="block"
     }
-
-    document.getElementById("myForm").style.display="block"
+    else if(monete==0)
+    {
+        openNoSuggerimenti()
+        document.getElementById("suggerimenti").disabled=true
+    }
 }
 function closeSuggerimenti()
 {
@@ -87,7 +102,7 @@ function utilizzaMonete()
         }
     if(monete==0)
     {
-        alert("Non hai abbastanza monete per poter utilizzare il suggerimento")
+
     }
 
 }
@@ -103,4 +118,22 @@ function assegnaMonete()
         document.getElementById("monete_gioco").innerHTML = localStorage.getItem("monete")
     }
 
+}
+function openRimuoviLettera(){
+    document.getElementById("myrimuoviletterapopup").style.display="block"
+}
+function closeRimuoviLettera(){
+    document.getElementById("myrimuoviletterapopup").style.display="none"
+}
+function pressOkRimuoviLettera()
+{
+    document.getElementById("").disabled=true//DA CONTINUARE
+}
+function openNoSuggerimenti()
+{
+    document.getElementById("mynosuggerimenti").style.display="block"
+}
+function closeNoSuggerimenti()
+{
+    document.getElementById("mynosuggerimenti").style.display="none"
 }
