806f1f0a25c53064a22ed5c2947f606c14cdb1ad
Salvatore-iovi <salvatore.iovinella001@studenti.uniparthenope.it>
2021-06-04 17:37:36 +0200

    non abbiamo ancora risolto il fatto dei tentativi aggiunti pero altri pop up

--- Liv1/Liv1.js
+++ Liv1/Liv1.js
@@ -1,106 +1,164 @@
 var cont=0 //variabile per verificare se si usano i suggerimenti
+var error=false
 var tentativi=3
-function controlloRisposta()
-{
-    let risposta_esatta="investigatore"
-    let display=document.getElementById("risposta_utente")
-    let displayValue=display.value
-    if(displayValue.toLowerCase()===risposta_esatta)
-    {
-        alert("Risposta esatta")
+var error=false
+function controlloRisposta() {
+    let risposta_esatta = "investigatore"
+    let display = document.getElementById("risposta_utente")
+    let displayValue = display.value
+    if (displayValue.toLowerCase() === risposta_esatta) {
+        openRispostaEsatta()
         assegnaPunteggio()
         assegnaMonete()
-        location.href="http://localhost:63342/GuessTheWord/Liv2/Livello2.html?_ijt=n2anun9vr4ue7j1g0vomjdtnf9"
-    }else{
+        document.getElementById("monete_gioco").innerHTML = monete
+    }if(displayValue!==risposta_esatta)/*DA VEDERE*/
+    {
         tentativi--
-        alert("Risposta errata...Ti restano: " +tentativi+ " tentativi")
-        display.value=""
+        localStorage.setItem("tentativo",tentativi)
+        openRispostaErrata()
+        document.getElementById(/*da vedere*/).innerHTML=localStorage.getItem("tentativo")
+        displayValue=""
 
-    if(tentativi==0)
-    {
-        alert("Hai perso")
-        location.href="http://localhost:63342/GuessTheWord/Index/index.html?_ijt=67i1nsgsb6dook9fnd981spm60"
     }
+     if (displayValue == ' ' || displayValue == "") {
+        openCaratteriMancanti()
+        display.value = ""
     }
 }
-function openSuggerimenti()
-{
-    cont=1
-    monete = monete - 50
-    localStorage.setItem("coins",monete)
-    document.getElementById("monete_gioco").innerHTML=monete
-    if(monete==0)
-    {
-        document.getElementById("suggerimenti").disabled=true
-        alert("Non hai abbastanza monete per poter utilizzare i suggerimenti")
+function openSuggerimenti() {
+    cont = 1
+
+    if (monete >= 50) {
+        monete = monete - 50
+        document.getElementById("myForm").style.display = "block"
+        localStorage.setItem("coins", monete)
+        document.getElementById("monete_gioco").innerHTML = monete
+    } else if (monete < 50) {
+        openNoSuggerimenti()
+    }
+}
+    function closeSuggerimenti() {
+        document.getElementById("myForm").style.display = "none"
     }
 
-    document.getElementById("myForm").style.display="block"
-}
-function closeSuggerimenti()
-{
-    document.getElementById("myForm").style.display="none"
-}
-function pressOk()
-{
-    location.href="http://localhost:63342/GuessTheWord/Index/index.html?_ijt=86v8kjac2fhh3m9r185tn6kg5f"
-}
-function openHome()
-{
-    document.getElementById("myhomepopup").style.display="block"
-}
-function closeHome()
-{
-    document.getElementById("myhomepopup").style.display="none"
-}
-function openRicomincia()
-{
-    document.getElementById("myricominciapopup").style.display="block"
-}
-function closeRicomincia()
-{
-    document.getElementById("myricominciapopup").style.display="none"
-}
-function pressOkRicomincia()
-{
-    location.href="http://localhost:63342/GuessTheWord/Liv1/Livello1.html?_ijt=jum6d1m4tuv1uvu7tlrli39599"
-}
-function assegnaPunteggio()
-{
-    if(cont == 1){
-        punti = punti + 50
-        localStorage.setItem("punteggio", punti)
-        document.getElementById("punti_container").innerHTML = localStorage.getItem("punteggio")
-    } else if(cont == 0){
-        punti = punti + 100
-        localStorage.setItem("punteggio", punti)
-        document.getElementById("punti_container").innerHTML = localStorage.getItem("punteggio")
+    function pressOk() {
+        location.href = "http://localhost:63342/GuessTheWord/Index/index.html?_ijt=86v8kjac2fhh3m9r185tn6kg5f"
     }
-}
-function utilizzaMonete()
-{
-        if(cont==1)
-        {
-            monete=monete-50
-            localStorage.setItem("coins",monete)
-            document.getElementById("monete_gico").innerHTML=localStorage.getItem(monete)
+
+    function openHome() {
+        document.getElementById("myhomepopup").style.display = "block"
+    }
+
+    function closeHome() {
+        document.getElementById("myhomepopup").style.display = "none"
+    }
+
+    function openRicomincia() {
+        document.getElementById("myricominciapopup").style.display = "block"
+    }
+
+    function closeRicomincia() {
+        document.getElementById("myricominciapopup").style.display = "none"
+    }
+
+    function pressOkRicomincia() {
+        location.href = "http://localhost:63342/GuessTheWord/Liv1/Livello1.html?_ijt=jum6d1m4tuv1uvu7tlrli39599"
+    }
+
+    function assegnaPunteggio() {
+        if (cont == 1) {
+            punti = punti + 50
+            localStorage.setItem("punteggio", punti)
+            document.getElementById("punti_container").innerHTML = localStorage.getItem("punteggio")
+        } else if (cont == 0) {
+            punti = punti + 100
+            localStorage.setItem("punteggio", punti)
+            document.getElementById("punti_container").innerHTML = localStorage.getItem("punteggio")
         }
-    if(monete==0)
+    }
+
+    function utilizzaMonete() {
+        if (cont == 1) {
+            monete = monete - 50
+            localStorage.setItem("coins", monete)
+            document.getElementById("monete_gico").innerHTML = localStorage.getItem(monete)
+        }
+        if (monete == 0) {
+
+        }
+
+    }
+
+    function assegnaMonete() {
+        if (cont == 1) {
+            monete = monete + 25
+            localStorage.setItem("coins", monete)
+            document.getElementById("monete_gioco").innerHTML = localStorage.getItem("coins")
+        } else if (cont == 0) {
+            monete = monete + 50
+            localStorage.setItem("coins", monete)
+            document.getElementById("monete_gioco").innerHTML = localStorage.getItem("monete")
+        }
+
+    }
+
+    function openRimuoviLettera() {
+        document.getElementById("myrimuoviletterapopup").style.display = "block"
+    }
+
+    function closeRimuoviLettera() {
+        document.getElementById("myrimuoviletterapopup").style.display = "none"
+    }
+
+    function pressOkRimuoviLettera() {
+        document.getElementById("").disabled = true//DA CONTINUARE
+    }
+
+    function openNoSuggerimenti() {
+        document.getElementById("mynosuggerimenti").style.display = "block"
+    }
+
+    function closeNoSuggerimenti() {
+        document.getElementById("mynosuggerimenti").style.display = "none"
+    }
+
+    function openRispostaEsatta() {
+        document.getElementById("myrispostaesatta").style.display = "block"
+    }
+
+    function closeRispostaEsatta() {
+        document.getElementById("myrispostaesatta").style.display = "none"
+        location.href = "http://localhost:63342/GuessTheWord/Liv2/Livello2.html?_ijt=n2anun9vr4ue7j1g0vomjdtnf9"
+    }
+
+    function openRispostaErrata() {
+        document.getElementById("myrispostaerrata").style.display = "block"
+    }
+
+    function closeRispostaErrata() {
+        document.getElementById("myrispostaerrata").style.display = "none"
+    }
+
+    function recuperaTentativi(valore) {
+        return valore
+    }
+    function openCaratteriMancanti()
     {
-        alert("Non hai abbastanza monete per poter utilizzare il suggerimento")
+        document.getElementById("mycaratterimancanti").style.display = "block"
     }
-
-}
-function assegnaMonete()
-{
-    if(cont == 1){
-        monete = monete + 25
-        localStorage.setItem("coins", monete)
-        document.getElementById("monete_gioco").innerHTML = localStorage.getItem("coins")
-    } else if(cont == 0){
-        monete = monete + 50
-        localStorage.setItem("coins", monete)
-        document.getElementById("monete_gioco").innerHTML = localStorage.getItem("monete")
+    function closeCaratteriMancanti()
+    {
+        document.getElementById("mycaratterimancanti").style.display = "none"
     }
-
-}
+    function openPartitaPersa()
+    {
+        document.getElementById("mypartitapersa").style.display = "block"
+    }
+    function yesPartitaPersa()
+    {
+        location.href = "http://localhost:63342/GuessTheWord/Liv1/Livello1.html?_ijt=1u1d0gkge0ttvf1l4j6flf4mvg"
+    }
+    function noPartitaPersa()
+    {
+        location.href = "http://localhost:63342/GuessTheWord/Index/index.html?_ijt=c6apgdbbfq3podof7b2sd7khu7"
+    }
